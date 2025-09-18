9d166e0502433c58d969e07849e47748e73970d6
Richard Perez Sofan <richard2003prezsofan@gmail.com>
2025-09-18 00:37:53 +0000

    sigue el mismo problema y cuando le doy en descargar sale esto

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.unicordoba.edu.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  experimental: {
+    // Increase the body size limit for server actions to 4.5MB
+    // This is to allow for larger payloads, like images, to be sent to server actions.
+    serverActions: {
+      bodySizeLimit: '4.5mb',
+    },
   },
+   serverActions: {
+      bodySizeLimit: '4.5mb',
+    },
 };
 
 export default nextConfig;
