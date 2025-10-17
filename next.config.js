88274a8426d4ce67fe4e716d4dbfe6c5b270e5bc
Richard Perez Sofan <richard2003prezsofan@gmail.com>
2025-10-17 02:29:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   srcDir: 'src/',
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
-  },
 };
 
 module.exports = nextConfig;
