94cd8372be9fceb82f643bb9fbada9631dae4494
Richard Perez Sofan <richard2003prezsofan@gmail.com>
2025-11-24 21:47:25 +0000

    Sobrenombre de la app edusparkai ID de la app 1:637462240048:web:f232354

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,19 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "eduspark-ai-837vs",
-  "appId": "1:637462240048:web:bcf0d49b43e8c4c08dc6f5",
-  "storageBucket": "eduspark-ai-837vs.firebasestorage.app",
-  "apiKey": "AIzaSyAAU1Al3JlmckfzEGD69mHd3y7WFv6TYaM",
-  "authDomain": "eduspark-ai-837vs.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "637462240048"
+  apiKey: "AIzaSyDy895-0XNjfY2_HvvzUJ8dMRs8Ss4WH-Q",
+  authDomain: "eduspark-ai-837vs.firebaseapp.com",
+  projectId: "eduspark-ai-837vs",
+  storageBucket: "eduspark-ai-837vs.firebasestorage.app",
+  messagingSenderId: "637462240048",
+  appId: "1:637462240048:web:f232354974c9a9298dc6f5"
 };
+
 
 // Initialize Firebase
 // To avoid re-initializing on hot reloads
