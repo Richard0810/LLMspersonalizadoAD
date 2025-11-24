4c57f7e8cc3e5acd347e344e2a664ddc93472ab2
Richard Perez Sofan <richard2003prezsofan@gmail.com>
2025-11-24 21:53:14 +0000

    Error Falló la generación de actividades. [GoogleGenerativeAI Error]: Er

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  }
 };
 
 module.exports = nextConfig;
